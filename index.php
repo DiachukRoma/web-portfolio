8a7e6b96c6a89495acf6ea0d89c3e305ef909126
DiachukRoma <roma19.05.94@gmail.com>
2019-02-04 18:50:36 +0200

    Create new portfolio

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="uk_UA">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Web-portfolio</title>
+	
+	<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="fonts/fonts.css">
+</head>
+<body>
+	<section class="home">
+		<div class="block">
+			<p>My name is Roman <br>I am a web-developer.</p>
+			<ul class="menu">
+				<li class="item" data-item='about'>About</li>
+				<li class="item" data-item='work'>Work</li>
+				<li class="item" data-item='contact'>Contact</li>
+			</ul>
+			<div class="social">
+				<div class="socIcon"><a href="#"><img src="img/telegram.svg" alt=""></a></div>
+				<div class="socIcon"><a href="#"><img src="img/instagram.svg" alt=""></a></div>
+				<div class="socIcon"><a href="#"><img src="img/facebook.svg" alt=""></a></div>
+			</div>
+		</div>
+		<div class="iteMenu"></div>
+		<div class="iteMenu"></div>
+		<div class="iteMenu"></div>
+	</section>
+</body>
+</html>
